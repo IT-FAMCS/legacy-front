40bbc66509576c0ed50c361ae4de4330794cc7ab
Anastasia Karpushevich <a.karpushevich.a@gmail.com>
2024-09-08 00:34:15 +0300

    feat: rewrite queries

--- src/constants/department-url.ts
+++ src/constants/department-url.ts
@@ -1,3 +1,5 @@
-export const CREATE_DEPARTMENT = `${process.env.REACT_APP_HAND_URL}/api/departments/create/`;
+export const CREATE_DEPARTMENT = `${process.env.REACT_APP_HAND_URL}/api/departs/create/`;
 
-export const GET_DEPARTMENT = `${process.env.REACT_APP_HAND_URL}/api/departments/`;
+export const GET_DEPARTMENT = `${process.env.REACT_APP_HAND_URL}/api/departs/{title}`;
+
+export const UPDATE_DEPARTMENT = `${process.env.REACT_APP_HAND_URL}/api/departs/{title}/`;
