71188d67f536b6ec4b17c6325ebd99abfa23f273
Anastasia Karpushevich <a.karpushevich.a@gmail.com>
2024-09-08 08:54:35 +0300

    feat: return correct urls for department

--- src/constants/department-url.ts
+++ src/constants/department-url.ts
@@ -1,5 +1,3 @@
-export const CREATE_DEPARTMENT = `${process.env.REACT_APP_HAND_URL}/api/departs/create/`;
+export const CREATE_DEPARTMENT = `${process.env.REACT_APP_HAND_URL}/api/departments/create/`;
 
-export const GET_DEPARTMENT = `${process.env.REACT_APP_HAND_URL}/api/departs/{title}`;
-
-export const UPDATE_DEPARTMENT = `${process.env.REACT_APP_HAND_URL}/api/departs/{title}/`;
+export const GET_DEPARTMENT = `${process.env.REACT_APP_HAND_URL}/api/departments/`;
