9d374206175ffb44dd9436a46c4049721dbdc6fc
Anastasia Karpushevich <a.karpushevich.a@gmail.com>
2024-09-08 20:52:17 +0300

    feat: creating env constant for a new bd

---
+++ src/constants/departs-url.ts
@@ -0,0 +1,6 @@
+export const GET_DEPART = `${process.env.REACT_APP_HAND_URL}/api/departs/{title}/`;
+export const UPDATE_DEPART = `${process.env.REACT_APP_HAND_URL}/api/departs/{title}/`;
+export const CREATE_DEPART = `${process.env.REACT_APP_HAND_URL}/api/departs/{title}/`;
+export const LIST_DEPART = `${process.env.REACT_APP_HAND_URL}/api/departs/list/`;
+
+///список отделов и проектов
